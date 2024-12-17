9d72cd49a1337f40575be5b259ac180fbfa40bb2
tonkla2548 <67041305101@udru.ac.th>
2024-12-17 13:44:07 +0700

    014HW

---
+++ 014HW.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+   int MNY[15]={50, 5, 20, 4, 5, 1, 2, 89, 4, 7, 44, 32, 10, 21, 2,}; 
+   for (int i=0; i<15; i++){
+    cout << " Day " << i+1 << " save " << MNY[i] << " baht ";
+    cout << endl;
+}
+
+}
